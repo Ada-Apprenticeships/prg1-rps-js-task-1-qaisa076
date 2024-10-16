b1e26d3ffee653d59d85a994db10c898c75c44ca
qaisa076 <qais.ahmad@ada.ac.uk>
2024-10-16 11:27:11 +0000

    Full function: phase 1 (All tests passed)

--- rps.js
+++ rps.js
@@ -1,5 +1,6 @@
+// Function to check who wins
 function rockPaperScissors(player1, player2) {
-  // Draw
+  // Players Drawing
   if (player1 === player2) {
     return "draw"
   }
@@ -15,6 +16,27 @@ function rockPaperScissors(player1, player2) {
   else if ((player1 === "scissors") && (player2 === "paper")) {
     return "player1"
   }
+  else if ((player1 === "lizard") && (player2 === "spock")) {
+    return "player1"
+  }
+  else if ((player1 === "spock") && (player2 === "scissors")) {
+    return "player1"
+  }
+  else if ((player1 === "rock") && (player2 === "lizard")) {
+    return "player1"
+  }
+  else if ((player1 === "paper") && (player2 === "spock")) {
+    return "player1"
+  }
+  else if ((player1 === "scissors") && (player2 === "lizard")) {
+    return "player1"
+  }
+  else if ((player1 === "lizard") && (player2 === "paper")){
+    return "player1"
+  }
+  else if ((player1 === "spock") && (player2 === "rock")){
+    return "player1"
+  }
   else {
     return "player2"
   }
@@ -27,8 +49,8 @@ module.exports = {
 
 // Letting user input whether they want rock paper or scissors
 const readline = require("readline-sync")
-let player1Input = readline.question("Rock, Paper or Scissors? ")
-let player2Input = readline.question("Rock, Paper or Scissors? ")
+let player1Input = readline.question("Rock, Paper, Scissors, Lizard or Spock? ")
+let player2Input = readline.question("Rock, Paper or Scissors, Lizard or Spock? ")
 
 // Outputting the winner
 console.log(rockPaperScissors(player1Input,player2Input))
