d5c25ea345970a1117c125beb2b483558367fc9b
qaisa076 <qais.ahmad@ada.ac.uk>
2024-10-16 11:03:02 +0000

    Rock paper scissors function: first draft

--- rps.js
+++ rps.js
@@ -3,6 +3,21 @@ function rockPaperScissors(player1, player2) {
   if (player1 === player2) {
     return "draw"
   }
+  // Rock beating scissors
+  else if ((player1 === "rock") && (player2 === "scissors")){
+    return "player 1"
+  }
+  // Paper beating rock
+  else if ((player1 === "paper") && (player2 === "rock")) {
+    return "player 1"
+  }
+  // Scissors beating paper
+  else if ((player1 === "scissors") && (player2 === "paper")) {
+    return "player 1"
+  }
+  else {
+    return "player 2"
+  }
 }
 
 
@@ -19,4 +34,3 @@ module.exports = {
   rockPaperScissors,
 }
 
-console.log(rockPaperScissors("rock","rock"))
