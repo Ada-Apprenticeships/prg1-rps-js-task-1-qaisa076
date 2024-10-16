2ed01c7f455e9e79947e33e8a5ad15970c65c618
qaisa076 <qais.ahmad@ada.ac.uk>
2024-10-16 13:57:16 +0000

    Switch to array/dictionary to shorten code

--- rps.js
+++ rps.js
@@ -1,56 +1,30 @@
 // Function to check who wins
 function rockPaperScissors(player1, player2) {
-  // Players Drawing
+  const options = ["rock","paper","scissors","spock","lizard"];
+  // The key here shows each choice and then what it would beat on the right
+  const winningOptions = {
+    rock: ['scissors', 'lizard'],
+    paper: ['rock', 'spock'],
+    scissors: ['paper', 'lizard'],
+    lizard: ['spock', 'paper'],
+    spock: ['scissors', 'rock'] 
+  };
   if (player1 === player2) {
     return "draw"
   }
-  // Rock beating scissors
-  else if ((player1 === "rock") && (player2 === "scissors")){
-    return "player1"
-  }
-  // Paper beating rock
-  else if ((player1 === "paper") && (player2 === "rock")) {
-    return "player1"
-  }
-  // Scissors beating paper
-  else if ((player1 === "scissors") && (player2 === "paper")) {
-    return "player1"
-  }
-  else if ((player1 === "lizard") && (player2 === "spock")) {
-    return "player1"
-  }
-  else if ((player1 === "spock") && (player2 === "scissors")) {
-    return "player1"
-  }
-  else if ((player1 === "rock") && (player2 === "lizard")) {
-    return "player1"
-  }
-  else if ((player1 === "paper") && (player2 === "spock")) {
-    return "player1"
-  }
-  else if ((player1 === "scissors") && (player2 === "lizard")) {
-    return "player1"
-  }
-  else if ((player1 === "lizard") && (player2 === "paper")){
-    return "player1"
-  }
-  else if ((player1 === "spock") && (player2 === "rock")){
-    return "player1"
+  //Player 1 is being used as a reference and player 2 is then being compared against player 1 to see if they win or lose
+  else if (winningOptions[player1].includes(player2)) { 
+    return "player1";
   }
   else {
-    return "player2"
+    return "player2";
   }
 }
+
+// Outputting the winner
+console.log(rockPaperScissors("rock","spock"))
 
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
 }
-
-// Letting user input whether they want rock paper or scissors
-const readline = require("readline-sync")
-let player1Input = readline.question("Rock, Paper, Scissors, Lizard or Spock? ")
-let player2Input = readline.question("Rock, Paper or Scissors, Lizard or Spock? ")
-
-// Outputting the winner
-console.log(rockPaperScissors(player1Input,player2Input))
